d29c7ded9cd5cb3739243a8ebd55d31b256972b4
gab-md <gabymulandarmawan@gmail.com>
2025-12-11 16:04:50 +0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,16 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
 class DashboardController extends Controller
 {
     public function index()
     {
-        $user = Auth::user();
-
-        // data is static for now
         return view('dashboard', [
             'caloriesConsumed' => 0,
             'caloriesBurned' => 0,
